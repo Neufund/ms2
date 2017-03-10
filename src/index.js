57ad1582451ad5ae150d259cedf3cffda68072a0
Leonid Logvinov <logvinov.leon@gmail.com>
2017-03-10 12:31:41 +0100

    Add App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, browserHistory} from 'react-router'
+import App from './app/App';
 import Index from './app/Index';
 import NotFound from './app/NotFound';
 import './index.css';
 
 ReactDOM.render((
         <Router history={browserHistory}>
-            <Route path="/" component={Index}/>
-            <Route path="*" component={NotFound}/>
+            <Route component={App}>
+                <Route path="/" component={Index}/>
+                <Route path="*" component={NotFound}/>
+            </Route>
         </Router>
     ),
     document.getElementById('root')
