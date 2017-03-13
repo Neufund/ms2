e3f3c77f860cd6db8fdf5233cb691db9435c9437
Tomek Banach <tomek@banciur.org>
2017-03-13 12:14:39 +0100

    Changes in footer

--- src/ui/footer/Index.js
+++ src/ui/footer/Index.js
@@ -4,7 +4,12 @@ import './Index.css';
 export default () => {
     return (
         <div className="Footer">
-            Ama foota
+            <div>
+                Contact: <a href="mailto:hello@neufund.org">hello@neufund.org </a>
+            </div>
+            <div>
+                NEUFUND
+            </div>
         </div>
     )
 };
