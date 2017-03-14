ca77e42fe950db1a2f1e80d07c374a51f473dd45
Tomek Banach <tomek@banciur.org>
2017-03-14 02:08:29 +0100

    Add Contracts sign page

--- src/app/Contracts.js
+++ src/app/Contracts.js
@@ -1,15 +1,36 @@
 import React from 'react';
 import ProgressBar from '../ui/ProgressBar';
+import Headline from '../ui/Headline';
+import ContractSign from '../ui/ContractSign';
 import './Contracts.css';
+import {
+    Step,
+    Stepper,
+    StepLabel
+} from 'material-ui/Stepper';
 
 export default () => {
     return (
         <div className="Contracts">
+
+            <Headline text="Welcome Mr Investor"/>
+            <div className="secondary-info">To invest in NEUFUND please follow this steps</div>
             <ProgressBar progress={20}/>
             <div className="column leftColumn">
-
+                <Stepper activeStep={0} orientation="vertical">
+                    <Step>
+                        <StepLabel>Sign your Limited Partner Agreement</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel>Identification</StepLabel>
+                    </Step>
+                    <Step>
+                        <StepLabel>Bank transfer</StepLabel>
+                    </Step>
+                </Stepper>
             </div>
             <div className="column rightColumn">
+                <ContractSign />
             </div>
         </div>
     )
---
+++ src/ui/ContractSign.js
@@ -0,0 +1,74 @@
+import './ContractSign.css';
+import React from 'react';
+import FontIcon from 'material-ui/FontIcon';
+import Dialog from 'material-ui/Dialog';
+import IconButton from 'material-ui/IconButton';
+import Checkbox from 'material-ui/Checkbox';
+import RaisedButton from 'material-ui/RaisedButton';
+import {Link} from 'react-router';
+
+export default class ContractSign extends React.Component {
+
+    state = {
+        open: false,
+    };
+
+    handleOpen = () => {
+        this.setState({open: true});
+    };
+
+    handleClose = () => {
+        this.setState({open: false});
+    };
+
+    render() {
+
+        const actions = [
+            <IconButton tooltip="Download">
+                <FontIcon className="material-icons">file_download</FontIcon>
+            </IconButton>,
+            <IconButton tooltip="Print">
+                <FontIcon className="material-icons">print</FontIcon>
+            </IconButton>,
+        ];
+
+        return (
+            <div className="ContractSign">
+                <h4>Sign your Limited Partner Agreement</h4>
+                <p>To see contracts click on the icons</p>
+                <div className="Contracts">
+                    <div className="Contract Paper" onClick={this.handleOpen}></div>
+                    <div>
+                        <FontIcon className="material-icons">drag_handle</FontIcon>
+                    </div>
+                    <div className="Contract Smart" onClick={this.handleOpen}></div>
+                </div>
+                <Dialog
+                    modal={false}
+                    open={this.state.open}
+                    onRequestClose={this.handleClose}
+                    autoScrollBodyContent={true}
+                    actions={actions}
+                >
+                    {this.contract}
+                </Dialog>
+                <div className="CheckboxWrapper">
+                    <Checkbox label="I have read and understood the limited"/>
+                </div>
+                <div className="SignButtonWrapper">
+                    <Link to="/kyc">
+                        <RaisedButton label="Sign" />
+                    </Link>
+                </div>
+            </div>
+        )
+    }
+    contract = <div>
+        <h4>Lorem ipsum dolor sit amet</h4>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+        <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+        <p>Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi. Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
+        <p>Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat. Duis autem vel eum iriure dolor in hendrerit in vulputate velit esse molestie consequat, vel illum dolore eu feugiat nulla facilisis at vero eros et accumsan et iusto odio dignissim qui blandit praesent luptatum zzril delenit augue duis dolore te feugait nulla facilisi.</p>
+    </div>;
+};
