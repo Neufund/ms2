88ab4f070789708582d63f6989d961219460cdbe
Tomek Banach <tomek@banciur.org>
2017-03-21 16:37:27 +0100

    Change header icon sizes and positions

--- src/ui/header/IcoHeader.js
+++ src/ui/header/IcoHeader.js
@@ -6,7 +6,6 @@ export default (props) => {
     return cms(__filename)(
         <div className="row topArea">
             <div className="col-xs-12 col-md-10 col-md-offset-1">
-
                 <div className="stateChanger" onClick={() => props.setIcoState("preico")}>preico</div>
                 <div className="stateChanger" onClick={() => props.setIcoState("countdown")}>countdown</div>
                 <div className="stateChanger" onClick={() => props.setIcoState("ico")}>ico</div>
@@ -17,11 +16,11 @@ export default (props) => {
                 <div className="Header">
                     <div className="Logo"></div>
                     <div className="UserSection">
-                        <div className="UserPanel">
-                            <i className="material-icons">account_circle</i>
-                            <i className="material-icons">details</i>
+                        <div className="UserPanel" onClick={() => {alert("this will open LPA, KYC")}}>
+                            <i className="material-icons iconUser">account_circle</i>
+                            <i className="material-icons iconArrow">details</i>
                         </div>
-                        <span className="Logout">LOGOUT</span>
+                        <span className="Logout" onClick={() => {alert("this will log you out")}}>LOGOUT</span>
                     </div>
                 </div>
             </div>
